e42c58e937ce57809e17ab6940176e5e5697a7c0
Duc Nguyen <20131039@student.hust.edu.vn>
2025-06-16 22:28:03 +0700

    gán lại token

--- main.go
+++ main.go
@@ -16,14 +16,14 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const (
+var (
 	MongoDBURI      = "mongodb+srv://hos=true&w=majority&appName=Cluster0"
 	DBName          = "moneyflow"
 	Collection      = "stock_code"
 	CollectionOrder = "orders"
 	WebsocketURL    = "wss://openapi.tcbs.com.vn/ws/thesis/v1/stream/normal"
-	Token           = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGlvcGVuLnRjYnMuY29tLnZuIiwiZXhwIjoxNzUwMTcwNTUxLCJqdGkiOiIiLCJpYXQiOjE3NTAwODQxNTEsInN1YiI6IjEwMDAwNzE3MDYyIiwic3ViVHlwZSI6ImN1c3RvbWVyIiwiY3VzdG9keUlEIjoiMTA1QzEyODkxNyIsInRjYnNJZCI6IjEwMDAwNzE3MDYyIiwic2Vzc2lvbklEIjoiMjJjNjU2YWItMjc1ZS00ZmMyLTljNDUtNmFlZGFkZDNmM2Q1IiwiY2xpZW50SUQiOiIxIiwic3Vic2NyaXB0aW9uIjoiYmFzaWMiLCJzY29wZSI6WyJib25kIiwiZnVuZCIsInN0b2NrIl0sInN0ZXB1cF9leHAiOjE3NTAxMTI5NTEsIm90cCI6IjAyOTE1MSIsIm90cFR5cGUiOiJUT1RQIiwib3RwU291cmNlIjoiVENJTlZFU1QiLCJvdHBTZXNzaW9uSWQiOiIyZTM3NTU2ZC03ZTIwLTQxOWItYWM5MC1kZGQ4ODg4ZGY1MTQiLCJhY2NvdW50VHlwZSI6InByaW1hcnkiLCJhY2NvdW50X3N0YXR1cyI6IjEiLCJlbWFpbCI6ImhvYW5nbWluaHRyaTk5QGdtYWlsLmNvbSIsInJvbGVzIjpbImN1c3RvbWVyIiwiQXBwbGljYXRpb24vT1BFTl9BUElfUElMT1QiXSwiY2xpZW50X2tleSI6Ik9MMEVWdE9XTDhISUVjaC9hV240MTlMQ2tBK0p5UXBYeW1naU9pRG1pSVdRMFFGcmFkc1RjKzBpNHZvRjdmWTUifQ.Ssq1eF1m7-JtcM1Kvohf901LmPitu8v6IdqGMgqIxnUA3CCI3h-Dk0DunJeDSlTVdtVgaddF4q4wayS8c8VVUpkukV-Bhb7hzJ59eYkzDLodcmkp9WL7W5qsGKJWLKu8YO_hyWTALcqISQqCaRb-aeuFQ9QsC5kXmrDApVarGeDKs81OCi2awcYwsvm4qWQwFJQvLmRiOV7fowKdoArYxQ_SpbQ8tI7c3KXRBibmMaZl86sDt1oLMcL8ARzbn29FKBGVibgILbNMnX71vmxoIeNtpXqqUzujVRfdkGD2PUh7p3KHhOOnW8AfPFyYv34ZtrJ-t0VMa5yNdQDLsODEUA"
-	BatchSize       = 2 // Số lượng bản ghi trong một batch
+	Token           = "" // sẽ gán sau khi lấy được
+	BatchSize       = 2
 )
 
 var (
@@ -85,8 +85,13 @@ func main() {
 	// client, err := mongo.Connect(context.TODO(), clientOptions)
 	otp := getUserInput("📥 Nhập OTP:")
 	fmt.Printf("✅ Bạn đã nhập: %s\n", otp)
-	token, err := GetAccessToken("10000717062-85bbf26d-7365-414f-ba3f-956a122c726b", otp)
-	fmt.Println("✅ Token lấy được:", token)
+
+	accessToken, err := GetAccessToken("10000717062-85bbf26d-7365-414f-ba3f-956a122c726b", otp)
+	if err != nil {
+		log.Fatalf("❌ Lỗi lấy token: %v", err)
+	}
+	Token = accessToken
+	fmt.Println("✅ Token lấy được:", Token)
 
 	if err != nil {
 		fmt.Println("❌ Không lấy được token:", err)
