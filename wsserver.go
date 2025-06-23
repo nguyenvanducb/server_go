4e0191f19699b8ad2de34854009aef67a7226307
Nguyen-Van-Duc-1995 <ducnguyen95hust@gmail.com>
2025-06-23 10:54:08 +0700

    update

--- wsserver.go
+++ wsserver.go
@@ -66,8 +66,8 @@ func startWebSocketServer() {
 
 	go handleMessages()
 
-	log.Println("🟢 WebSocket server đang chạy tại ws://localhost:90/ws")
-	if err := http.ListenAndServe(":90", nil); err != nil {
+	log.Println("🟢 WebSocket server đang chạy tại ws://localhost:80/ws")
+	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal("❌ Lỗi khi chạy WebSocket server:", err)
 	}
 }
