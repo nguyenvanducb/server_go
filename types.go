524bc1176998296191460aff9cfc47ecd0d85318
Duc Nguyen <20131039@student.hust.edu.vn>
2025-07-10 23:39:20 +0700

    test

--- types.go
+++ types.go
@@ -2,9 +2,9 @@ package main
 
 // Danh sách nhóm chứng khoán theo từng nhóm
 var stockGroups = [][]string{
-	{"ACB", "BCM", "BID", "CTG", "TCB", "VCB", "VHM", "VIB", "SSI", "STB"},
-	{"FPT", "GAS", "GVR", "HDB", "HPG", "SAB", "SHB", "SSB", "TPB", "BVH"},
-	{"LPB", "MBB", "MSN", "MWG", "PLX", "VIC", "VJC", "VNM", "VPB", "VRE"},
+	{"SSI"},
+	{"SHB"},
+	{"MWG"},
 }
 
 // Chứa tất cả mã cổ phiếu gộp từ các nhóm (biến toàn cục)
